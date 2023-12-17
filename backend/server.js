b4385ab0123755453360bf4b26811bd57fe8f46e
Sresta Malla <mallasresta@gmail.com>
2023-12-18 02:25:01 +0545

    morgan third party logger middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
+const morgan = require("morgan");
 //bring in route file
 const bootcamps = require("./routes/bootcamps");
 
@@ -9,8 +9,13 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 //Mount the routes
 app.use("/api/v1/bootcamps", bootcamps);
+
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
